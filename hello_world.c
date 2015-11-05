b066249ff5dd6c4577a732181f1da2a96cdb7adc
Richard Batty <rjlbatty@gmail.com>
2015-11-05 19:44:20 +0000

    Initial commit

---
+++ hello_world.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#endif
+
+int main(int argc, char** argv) {
+
+  puts("Lispy Version 0.0.0.0.1");
+  puts("From http://buildyourownlisp.com/");
+  puts("Press Ctrl+c to Exit\n");
+
+  while(1) {
+    char* input = readline("battylisp> ");
+
+    add_history(input);
+    printf("You think you're too good for a %s\n", input);
+    free(input);
+  }
+  return 0;
+}
