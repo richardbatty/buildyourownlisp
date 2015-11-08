e3e1758c9ccdc4d4fcd680991831d4d7637c175a
Richard Batty <rjlbatty@gmail.com>
2015-11-08 13:21:30 +0000

    Add some more math functions: %, ^, min, max, and unary -

--- parsing.c
+++ parsing.c
@@ -34,6 +34,15 @@ long calculate(long x, char* op, long y) {
   if (strcmp(op, "-") == 0) { return x - y; }
   if (strcmp(op, "*") == 0) { return x * y; }
   if (strcmp(op, "/") == 0) { return x / y; }
+  if (strcmp(op, "%") == 0) { return x % y; }
+  if (strcmp(op, "^") == 0) { return pow(x, y); }
+  if (strcmp(op, "min") == 0) { return fmin(x, y); }
+  if (strcmp(op, "max") == 0) { return fmax(x, y); }
+  return 0;
+}
+
+long calculate_unary(char* op, long x) {
+  if (strcmp(op, "-") == 0) { return - x; }
   return 0;
 }
 
@@ -63,8 +72,14 @@ long eval(mpc_ast_t* ast_node) {
 
   char* op = operator(ast_node);
 
+  /* TODO: Create an array of operands to manipulate rather than
+  the awkward off-by-one ast node once I know more about pointers */
   /* We store the third child in `result` */
   long result = eval(nth_child(ast_node, 2));
+
+  if (!node_is_type(nth_child(ast_node, 3), "expr")) {
+    result = calculate_unary(op, result);
+  }
 
   /* Iterate the remaining children and combining. */
   int i = 3;
@@ -90,7 +105,7 @@ int main(int argc, char** argv) {
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                     \
       number   : /-?[0-9]+.?/ ;                           \
-      operator : '+' | '-' | '*' | '/' | '%' ;            \
+      operator : '+' | '-' | '*' | '/' | '%' | '^' | /min/ | /max/ ;      \
       expr     : <number> | '(' <operator> <expr>+ ')' ;  \
       lispy    : /^/ <operator> <expr>+ /$/ ;             \
     ",
