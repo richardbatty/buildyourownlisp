11d8305b55545f40791eb2d4edd26501c4299887
Richard Batty <rjlbatty@gmail.com>
2015-11-08 14:50:23 +0000

    Get rid of windows-specific stuff. I'll fix it for windows if it needs it.

--- src/parsing.c
+++ src/parsing.c
@@ -1,33 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <editline/readline.h>
 
 #include "../lib/mpc/mpc.h"
 #include "eval.h"
-
-/* If we are compiling on Windows compile these functions */
-#ifdef _WIN32
-#include <string.h>
-
-static char buffer[2048];
-
-/* Fake readline function */
-char* readline(char* prompt) {
-  fputs(prompt, stdout);
-  fgets(buffer, 2048, stdin);
-  char* cpy = malloc(strlen(buffer)+1);
-  strcpy(cpy, buffer);
-  cpy[strlen(cpy)-1] = '\0';
-  return cpy;
-}
-
-/* Fake add_history function */
-void add_history(char* unused) {}
-
-/* Otherwise include the editline headers */
-#else
-#include <editline/readline.h>
-#endif
 
 int main(int argc, char** argv) {
 
